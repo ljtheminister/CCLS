824aa1f6e9d7321424ea64d8eb8dccd510e5c47d
John Min <jcm2199@columbia.edu>
2013-11-15 21:45:54 -0500

    commit for some progress - need to test single versus dictionary version

--- getNDFD_Data.py
+++ getNDFD_Data.py
@@ -1,4 +1,4 @@
-import subprocess, os
+import subprocess, os, pickle
 
 def getdata(lat,lon,z,link,filename):
     """bla bla bla"""
@@ -15,17 +15,32 @@ def getdata(lat,lon,z,link,filename):
     dc1.exportCsvAllTimes(filename)
     return
 
-if __name__ == '__main__':
-    x,y,z = 40.78,-73.97,154.0
-
-    table = sys.argv[1]
+def parseInput(table):
     timestamp = table[-12:]
     url_date = timestamp[:6] + '/' + timestamp[:8] + '/'
-    url = base_url + url_date + table
-    filename = base_file + '/' + table + '.csv'
     base_url = 'http://nomads.ncdc.noaa.gov/thredds/dodsC/ndfd/'
-    
-    getdata(x,y,z)
+    url = base_url + url_date + table
+    base_file = os.getcwd()
+    filename = base_file + '/' + table + '.csv'
+    return (url, filename)
 
+def retrieveList(var):
+    table_list = pickle.load(open(var+'.p'))
+    return table_list
+
+
+if __name__ == '__main__':
+    x,y,z = 40.78,-73.97,154.0
+    '''
+    url, filename = parseInput(os.getenv('table'))
+    print url
+    print filename
+    getdata(x,y,z, url, filename)
+    '''
+
+    table_list = retrieveList(os.getenv('wx_var'))
+    for table in table_list:
+	url, filename = parseInput(table)
+	getdata(x,y,z, url, filename)
 
 
